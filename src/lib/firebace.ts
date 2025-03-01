9e81023a24447af10c3285b38221609e3826230f
nekit2123 <dorofeevn568@gmail.com>
2025-03-01 12:28:28 +0200

    Фикс помилок

--- src/lib/firebace.ts
+++ src/lib/firebace.ts
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
+\// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnTCikhvfuejYJAkqui9ZsmDslO1cCtI0",
-  authDomain: "studhelp-19970.firebaseapp.com",
-  projectId: "studhelp-19970",
-  storageBucket: "studhelp-19970.firebasestorage.app",
-  messagingSenderId: "855366134391",
-  appId: "1:855366134391:web:dc17be060e37a125cf3472",
-  measurementId: "G-JLD2S7V403"
+  apiKey: "AIzaSyC8eyBSgM6iahzUShGYp1gPAEKpoxhlFUA",
+  authDomain: "studenthelper-9253d.firebaseapp.com",
+  projectId: "studenthelper-9253d",
+  storageBucket: "studenthelper-9253d.firebasestorage.app",
+  messagingSenderId: "680561293503",
+  appId: "1:680561293503:web:0e6bd0154f3ec9d360ee0a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
