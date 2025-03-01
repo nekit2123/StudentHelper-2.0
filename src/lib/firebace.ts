57efdaf1c087ec998c746e021ee5d09e7e8676ca
nekit2123 <dorofeevn568@gmail.com>
2025-03-01 12:35:49 +0200

    fix

--- src/lib/firebace.ts
+++ src/lib/firebace.ts
@@ -1,16 +1,16 @@
-\// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8eyBSgM6iahzUShGYp1gPAEKpoxhlFUA",
-  authDomain: "studenthelper-9253d.firebaseapp.com",
-  projectId: "studenthelper-9253d",
-  storageBucket: "studenthelper-9253d.firebasestorage.app",
-  messagingSenderId: "680561293503",
-  appId: "1:680561293503:web:0e6bd0154f3ec9d360ee0a"
+  apiKey: "AIzaSyDfSjtJniybbfGX7lFN9MUT5G39SXSgTb0",
+  authDomain: "studenthelper-edf5f.firebaseapp.com",
+  projectId: "studenthelper-edf5f",
+  storageBucket: "studenthelper-edf5f.firebasestorage.app",
+  messagingSenderId: "1064260044149",
+  appId: "1:1064260044149:web:fa31fbbeb224a4cff9bf3c"
 };
 
 // Initialize Firebase
